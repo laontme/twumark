7b18801e6b3b060843893975b08a4c62ebbf7ed1
Stanislav Smirnov <stanislav@laont.me>
2020-12-12 17:14:05 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+document.getElementsByTagName('button')[0].addEventListener('click', getReason);
+
+function getReason() {
+  let crashArray = document.getElementById('raw').value.split('\n');
+  const exceptionAddress = parseInt(crash[1].replace('Exception At Address: ', ''));
+
+  let modulesLine;
+
+  crash.forEach((el, i) => {
+    if (el.trim() == 'Loaded Modules:') {
+      modulesLine = i + 1;
+    }
+  });
+
+  crash.splice(0, modulesLine);
+  crash.pop();
+  let crashNew = [];
+
+  crash.forEach((el, i) => {
+    crashNew.push(el.split('\t'));
+  });
+  crash = undefined;
+  crash = [];
+  crashNew.forEach((el) => {
+    el.shift();
+    el.b = parseInt(el[0].replace('B: ', ''));
+    el.shift();
+    el.s = parseInt(el[0].replace('S: ', ''));
+    el.shift();
+    el.path = el[0].replace('\r', '');
+    el.shift();
+    crash.push(Object.assign({}, el));
+  });
+
+  crash.forEach((el, i) => {
+    if (exAddr > el.b) {
+      if (exAddr < el.b + el.s) {
+        console.log(el.path);
+        document.getElementById('reason').value = el.path;
+      }
+    }
+  });
+}
